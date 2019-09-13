05e16480fb4bdd8636af2c1cdf4a89d2ca2832c9
José Pedro <zemiguel.apedro@gmail.com>
2019-09-13 02:38:47 +0100

    linkin park 2.0

--- server.py
+++ server.py
@@ -115,12 +115,12 @@ def get_device_queue(master_id):
 
     if not result or not result[0]:
         cursor.close()
-        return ('DO NOTHING', 200, 'text/plain')
+        return 'DO NOTHING'
     else:
         query = 'UPDATE masters_messages SET message=NULL WHERE master_id=\'{}\''.format(master_id)
         cursor.execute(query)
         cursor.close()
-        return (result[0], 200, 'text/plain')
+        return result[0]
 
 @app.route('/devices/<string:unique_id>/<string:identifier>', methods = ['PUT'])
 def unlock_device(unique_id, identifier):
